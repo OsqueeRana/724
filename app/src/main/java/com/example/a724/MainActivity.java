a4c6308522c58e5e3dd7b8313d568738c0e0a638
OsqueeRana <osqueerana@gmail.com>
2023-06-14 18:45:18 +0330

    mainActivity

--- app/src/main/java/com/example/a724/MainActivity.java
+++ app/src/main/java/com/example/a724/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.a724;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView ghabz;
+    ImageView charge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ghabz = findViewById(R.id.imageView2);
+        charge = findViewById(R.id.imageView3);
+
+        ghabz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GhabzActivity.class);
+                startActivity(intent);
+            }
+        });
+        charge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ChargeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
