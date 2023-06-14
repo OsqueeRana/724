40fcd489624ef7a47465d92a702c4c42da223291
OsqueeRana <osqueerana@gmail.com>
2023-06-14 18:44:57 +0330

    add activities

---
+++ app/src/main/java/com/example/a724/GhabzActivity.java
@@ -0,0 +1,14 @@
+package com.example.a724;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GhabzActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ghabz);
+    }
+}
